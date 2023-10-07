74b991b1cdf9d335859048c8caef2e7ae886c3d0
Uyen <140712857+Uyennttu@users.noreply.github.com>
2023-10-07 17:26:02 +1000

    Add files via upload
    final demo1 and 2

--- demo1.py
+++ demo1.py
@@ -4,37 +4,83 @@ import pandas as pd
 import threading
 from datetime import datetime
 
-# Load dataset
-dataset = pd.read_csv('listings_dec18.csv')
+
+def load_data():
+    global listings_dataset, calendar_dataset
+    try:
+        listings_dataset = pd.read_csv('listings_dec18.csv', low_memory=False)
+        calendar_dataset = pd.read_csv('calendar_dec18.csv')
+        calendar_dataset['date'] = pd.to_datetime(calendar_dataset['date'], errors='coerce')
+    except Exception as e:
+        print(f"Error loading data: {e}")
 
 
-def report_all_listings(start_date, end_date, search_suburb, output_text):
+def report_all_listings(start_date, end_date, search_suburb, output_text, report_button):
+    report_button.config(state=tk.DISABLED)
+    output_text.insert(tk.END, "Searching...\n")
     try:
         start_date = datetime.strptime(start_date, '%Y-%m-%d')
         end_date = datetime.strptime(end_date, '%Y-%m-%d')
     except ValueError:
         output_text.insert(tk.END, "Invalid date format. Please use YYYY-MM-DD format.\n")
         return
+
     if not search_suburb:
         output_text.insert(tk.END, "Please ensure suburb field is filled.\n")
         return
+
     if start_date > end_date:
         output_text.insert(tk.END, "Date range is not in order. Please check date order.\n")
         return
 
-    output_text.insert(tk.END, "Searching...\n")
-
-    # Convert 'last_scraped' to datetime
-    dataset['last_scraped'] = pd.to_datetime(dataset['last_scraped'], errors='coerce')
-
     # Filter rows using vectorized operations
-    mask = (dataset['last_scraped'] >= start_date) & (dataset['last_scraped'] <= end_date) & (dataset['neighbourhood'].str.contains(search_suburb, case=False, na=False))
-    filtered_data = dataset.loc[mask]
+    mask = (calendar_dataset['date'] >= start_date) & (calendar_dataset['date'] <= end_date)
+    filtered_calendar_data = calendar_dataset.loc[mask]
+
+    # Merge the listings and calendar datasets on the 'listing_id' column
+    merged_data = pd.merge(filtered_calendar_data, listings_dataset, left_on='listing_id', right_on='id', how='inner')
+
+    # Filter the merged data by suburb
+    mask = merged_data['neighbourhood'].str.contains(search_suburb, case=False, na=False)
+    filtered_data = merged_data.loc[mask]
+
+    # Ensure 'date' and 'price' is available
+    if 'date' not in filtered_data or 'price_x' not in filtered_data:
+        output_text.insert(tk.END, "Unexpected columns after data merge, please check datasets.\n")
+        return
 
     # Sort and Display the filtered data
-    sorted_data = filtered_data.sort_values(by='last_scraped')
+    sorted_data = filtered_data.sort_values(by='date')
+
+    if sorted_data.empty:
+        output_text.insert(tk.END, "No results found.\n")
+        return
+
+    # Columns related to reviews and comments
+    excluded_columns = [
+        'summary', 'space', 'description', 'neighborhood_overview', 'notes', 'transit',
+        'access', 'interaction', 'house_rules', 'review_scores_rating',
+        'review_scores_accuracy', 'review_scores_cleanliness',
+        'review_scores_checkin', 'review_scores_communication',
+        'review_scores_location', 'review_scores_value', 'number_of_reviews',
+        'first_review', 'last_review', 'reviews_per_month'
+    ]
+
     for index, row in sorted_data.iterrows():
-        output_text.insert(tk.END, f"Name: {row['name']}, Suburb: {row['neighbourhood']}, Date: {row['last_scraped'].strftime('%Y-%m-%d')}, Price: {row['price']}\n")
+        output_msg = "Listing Details:\n"
+
+        # Loop through all columns and add to output if not excluded
+        for col in sorted_data.columns:
+            if col not in excluded_columns:
+                output_msg += f"{col.replace('_', ' ').capitalize()}: {row[col]}\n"
+
+        # Insert the constructed message into the Text widget
+        output_text.insert(tk.END, output_msg + "\n---\n")
+
+        # To prevent application freezing during long iterations
+        app.update_idletasks()
+
+    report_button.config(state=tk.NORMAL)
 
 
 app = tk.Tk()
@@ -67,18 +113,25 @@ search_suburb_entry.grid(row=2, column=1, padx=5, pady=5)
 # Output Text
 output_text = tk.Text(frame1, wrap='word', height=10, width=50)
 output_text.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+
+scrollbar = tk.Scrollbar(frame1, command=output_text.yview)
+scrollbar.grid(row=4, column=2, sticky='nsew')
+
+output_text['yscrollcommand'] = scrollbar.set
+
 output_text.insert(tk.END, "Results will be displayed here.\n")
 
-# Report Button
+# Modify Report Button
 report_button = ttk.Button(
     frame1,
     text="Generate Report",
     command=lambda: threading.Thread(
         target=report_all_listings,
-        args=(start_date_entry.get(), end_date_entry.get(), search_suburb_entry.get(), output_text),
+        args=(start_date_entry.get(), end_date_entry.get(), search_suburb_entry.get(), output_text, report_button),
         daemon=True
     ).start()
 )
 report_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
 
+load_data()
 app.mainloop()
--- demo2.py
+++ demo2.py
@@ -1,99 +1,79 @@
-import tkinter as tk
-from tkinter import ttk
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from datetime import datetime
+import tkinter as tk
+from tkinter import ttk, messagebox  # Added messagebox for user alerts
 
-# Load the dataset and suppress low_memory warning
-dataset = pd.read_csv('listings_summary_dec18.csv', low_memory=False)
+
+def load_data():
+    listings = pd.read_csv('listings_dec18.csv', low_memory=False)
+    calendar = pd.read_csv('calendar_dec18.csv', parse_dates=['date'], low_memory=False)
+    return listings, calendar
 
 
-def display_prices(start_date, end_date, frame, top_n):
-    try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        end_date = datetime.strptime(end_date, '%Y-%m-%d')
-    except ValueError:
-        print("Invalid date format, please enter dates in YYYY-MM-DD format.")
+def display_prices(start_date, end_date, frame, top_n, listings_df, calendar_df):
+    listings_subset = listings_df[['id', 'neighbourhood']]
+    calendar_subset = calendar_df[
+        (calendar_df['date'] >= start_date) & (calendar_df['date'] <= end_date)
+        ][['listing_id', 'date', 'price']]
+    calendar_subset['price'] = calendar_subset['price'].replace('[\$,]', '', regex=True).astype(float)
+
+    merged_data = pd.merge(calendar_subset, listings_subset, left_on='listing_id', right_on='id', how='inner')
+    suburb_means = merged_data.groupby('neighbourhood')['price'].mean()
+    sorted_suburb_means = suburb_means.sort_values(ascending=False).head(top_n)
+
+    if sorted_suburb_means.empty:
+        messagebox.showerror("Error", "No data to plot.")  # GUI feedback
         return
-
-    if start_date > end_date:
-        print("Start date cannot be greater than end date.")
-        return
-
-    suburb_prices = {}
-    for index, row in dataset.iterrows():
-        listing_date = datetime.strptime(row['last_scraped'], '%Y-%m-%d')
-        listing_suburb = str(row['neighbourhood']) if pd.notna(row['neighbourhood']) else 'Unknown'
-        listing_price = row['price']
-
-        if start_date <= listing_date <= end_date:
-            if listing_suburb not in suburb_prices:
-                suburb_prices[listing_suburb] = {'total_price': 0, 'count': 0}
-
-            suburb_prices[listing_suburb]['total_price'] += float(listing_price.strip('$').replace(',', ''))
-            suburb_prices[listing_suburb]['count'] += 1
-
-    suburb_means = {suburb: data['total_price'] / data['count'] for suburb, data in suburb_prices.items()}
-    sorted_suburb_means = dict(sorted(suburb_means.items(), key=lambda item: item[1], reverse=True)[:top_n])
-
-    if not sorted_suburb_means:
-        print("No data to plot.")
-        return
-
-    suburbs = list(sorted_suburb_means.keys())
-    mean_prices = list(sorted_suburb_means.values())
 
     fig, ax = plt.subplots(figsize=(8, 4))
-    ax.barh(suburbs, mean_prices)
-    ax.set(xlabel='Mean Price ($)', ylabel='Suburb', title='Mean Prices by Suburb from {} to {}'.format(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
-    ax.invert_yaxis()  # To display the highest value at the top in the horizontal bar chart
+    ax.barh(sorted_suburb_means.index, sorted_suburb_means.values)
+    ax.set(xlabel='Mean Price ($)', ylabel='Suburb',
+           title='Mean Prices by Suburb from {} to {}'.format(start_date.strftime('%Y-%m-%d'),
+                                                              end_date.strftime('%Y-%m-%d')))
+    ax.invert_yaxis()
 
-    canvas = FigureCanvasTkAgg(fig, master=frame)  # A tk.DrawingArea
+    canvas = FigureCanvasTkAgg(fig, master=frame)
     canvas.draw()
     canvas.get_tk_widget().grid(row=4, column=0, columnspan=2, padx=5, pady=5)
 
 
-# GUI Setup
-app = tk.Tk()
-app.title("Airbnb Data Analysis")
-
-notebook = ttk.Notebook(app)
-notebook.pack(padx=10, pady=10)
-
-frame2 = ttk.Frame(notebook)
-notebook.add(frame2, text="Display Prices")
-
-# Start Date
-ttk.Label(frame2, text="Start Date (YYYY-MM-DD):").grid(row=0, column=0, padx=5, pady=5)
-start_date_entry_2 = ttk.Entry(frame2)
-start_date_entry_2.grid(row=0, column=1, padx=5, pady=5)
-
-# End Date
-ttk.Label(frame2, text="End Date (YYYY-MM-DD):").grid(row=1, column=0, padx=5, pady=5)
-end_date_entry_2 = ttk.Entry(frame2)
-end_date_entry_2.grid(row=1, column=1, padx=5, pady=5)
-
-# Top N Dropdown
-ttk.Label(frame2, text="Top N Suburbs:").grid(row=2, column=0, padx=5, pady=5)
-top_n_values = [str(i) for i in range(1, 21)]
-top_n_var = tk.StringVar(value=top_n_values[9])
-top_n_dropdown = ttk.Combobox(frame2, textvariable=top_n_var, values=top_n_values)
-top_n_dropdown.grid(row=2, column=1, padx=5, pady=5)
-
-# Display Prices Button
-display_prices_button = ttk.Button(
-    frame2,
-    text="Display Prices",
-    command=lambda: display_prices(
-        start_date_entry_2.get(),
-        end_date_entry_2.get(),
-        frame2,
-        int(top_n_var.get())
-    )
-)
-display_prices_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
-
-app.mainloop()
+def on_submit():
+    try:
+        start_date = datetime.strptime(start_date_entry.get(), "%Y-%m-%d")
+        end_date = datetime.strptime(end_date_entry.get(), "%Y-%m-%d")
+        top_n = int(top_n_entry.get())
+        display_prices(start_date, end_date, frame, top_n, listings, calendar)
+    except ValueError as ve:
+        messagebox.showerror("Error", "Invalid input: {}".format(ve))  # GUI feedback
 
 
+# Load data
+listings, calendar = load_data()
+
+root = tk.Tk()
+root.title('Price Visualization')
+
+frame = ttk.Frame(root, padding='5 5 5 5')
+frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(0, weight=1)
+
+ttk.Label(frame, text='Start date (YYYY-MM-DD):').grid(column=0, row=0, sticky=tk.W)
+start_date_entry = ttk.Entry(frame, width=15)
+start_date_entry.grid(column=1, row=0, sticky=(tk.W, tk.E))
+
+ttk.Label(frame, text='End date (YYYY-MM-DD):').grid(column=0, row=1, sticky=tk.W)
+end_date_entry = ttk.Entry(frame, width=15)
+end_date_entry.grid(column=1, row=1, sticky=(tk.W, tk.E))
+
+ttk.Label(frame, text='Top N neighbourhoods:').grid(column=0, row=2, sticky=tk.W)
+top_n_entry = ttk.Entry(frame, width=5)
+top_n_entry.grid(column=1, row=2, sticky=(tk.W, tk.E))
+top_n_entry.insert(0, '5')
+
+submit_button = ttk.Button(frame, text='Submit', command=on_submit)
+submit_button.grid(column=1, row=3, sticky=tk.W)
+
+root.mainloop()
